5c83628e234d3b0679b1f5761d33bae02cc6a066
jrodalo <jrodalo@gmail.com>
2016-07-11 16:55:57 +0100

    Cambios en el aspecto de los checks

--- resources/assets/js/components/list-item.vue
+++ resources/assets/js/components/list-item.vue
@@ -55,8 +55,11 @@
 	.item__name {flex: 1; padding: 10px;}
 	.item__toggle {display: block; position: relative; padding-left: 15px; cursor: pointer;	font-size: 18px;}
 	.item__checkbox {opacity: 0;}
+	.item__indicator {position: absolute; top: 2px; left: 0; height: 20px; width: 20px; background: #FFF; border: 1px solid darken($back-color, 7); border-radius: 3px;}
+	.item__checkbox:checked ~ .item__indicator {background: $dark-color; border: 0;}
+	.item__checkbox:active ~ .item__indicator {background: $dark-color;}
+	.item__checkbox:focus ~ .item__indicator {border-color: $dark-color;}
 	.item__checkbox:checked ~ .item__indicator::after {display: block;}
-	.item__indicator {position: absolute; top: 2px; left: 0; height: 20px; width: 20px; background: desaturate($light-color, 15); border-radius: 3px;}
 	.item__indicator::after {
 		content: '';
 		position: absolute;
