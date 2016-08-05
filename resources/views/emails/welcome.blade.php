d9603685ddd1e29702fa24baf678f88dc5097bc1
jrodalo <jrodalo@gmail.com>
2016-08-05 19:45:40 +0100

    Corregido un poco el texto del email de bienvenida

--- resources/views/emails/welcome.blade.php
+++ resources/views/emails/welcome.blade.php
@@ -1,5 +1,7 @@
-Bienvenid@ a Sopplis.com
+<h1>Bienvenid@ a Sopplis.com</h1>
 
-<a href="http://sopplis.dev/login?token={{ $user->remember_token }}">Entrar en Sopplis.com</a>
+<p>Muchas gracias por registrarte en Sopplis.</p>
 
-<small>Has recibido este correo porque alguien a introducido tu email en sopplis.com. Si no has sido tu puedes ignorar este correo.</small>
+<p><b><a href="{{ config('app.url') }}/login?token={{ $user->remember_token }}">Entrar en sopplis.com</a></b></p>
+
+<p><small>Has recibido este correo porque alguien a introducido tu email en sopplis.com. Si no has sido tu puedes ignorar este correo.</small></p>
