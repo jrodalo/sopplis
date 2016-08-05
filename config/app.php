267df47747e4c98641070d076605a50054938073
jrodalo <jrodalo@gmail.com>
2016-08-05 23:41:22 +0100

    Añadida la librería de Bugsnag para gestionar los errores

--- config/app.php
+++ config/app.php
@@ -161,6 +161,8 @@ return [
         // DebugBar
         //Barryvdh\Debugbar\ServiceProvider::class,
 
+        // Bugsnag
+        Bugsnag\BugsnagLaravel\BugsnagServiceProvider::class,
     ],
 
     /*
@@ -206,6 +208,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Bugsnag' => Bugsnag\BugsnagLaravel\Facades\Bugsnag::class,
 
     ],
 
