dd928da97c04ec4e5d31c6701d2c6baa925fd7fe
jrodalo <jrodalo@gmail.com>
2016-08-06 17:13:35 +0100

    Mejorada la página del 404

--- resources/assets/js/pages/404.vue
+++ resources/assets/js/pages/404.vue
@@ -1,25 +1,19 @@
 <template>
 
-	<section id="login" class="page">
+	<section id="notfound" class="page">
+
+		<header class="header">
+			<div class="header__content">
+				<a v-link="{ name: 'home', params: {} }" class="header__button">«</a>
+				<h1 class="header__title">Página no encontrada</h1>
+			</div>
+		</header>
 
 		<div class="content content--centered">
-
 			<h1 class="title">Upss!</h1>
-
 			<p>Parece que la página que buscas no existe o ha cambiado de sitio...</p>
-
-			<p><a v-link="{ name: 'home', params: {}}">Volver al inicio</a></p>
-
 		</div>
 
 	</section>
 
 </template>
-
-<script>
-
-	export default {
-
-	};
-
-</script>
