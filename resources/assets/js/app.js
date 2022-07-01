6ede688337524301cdf78bf5dc1590142d28a75f
Shift <shift@laravelshift.com>
2022-07-01 10:46:25 +0000

    Rename ENV variables

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -33,8 +33,8 @@ import Pusher from 'pusher-js';
 window.Pusher = Pusher;
 window.Echo = new Echo({
     broadcaster: 'pusher',
-    key: process.env.MIX_PUSHER_APP_KEY,
-    cluster: process.env.MIX_PUSHER_APP_CLUSTER,
+    key: import.meta.env.VITE_PUSHER_APP_KEY,
+    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
     auth: store.getters.authenticationHeaders,
 });
 
