fb7ac990ffaadcb5bcf590e90abbf7ff31c77d74
Shift <shift@laravelshift.com>
2022-07-01 10:46:25 +0000

    Use ES modules

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -3,7 +3,8 @@ import VueRouter from 'vue-router';
 import Echo from "laravel-echo";
 import store from './store';
 
-window.axios = require('axios');
+import axios from 'axios';
+window.axios = axios;
 window.axios.defaults.baseURL='/api/v1';
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
@@ -28,7 +29,8 @@ window.axios.interceptors.response.use(response => response, error => {
     return Promise.reject(error.response);
 });
 
-window.Pusher = require('pusher-js');
+import Pusher from 'pusher-js';
+window.Pusher = Pusher;
 window.Echo = new Echo({
     broadcaster: 'pusher',
     key: process.env.MIX_PUSHER_APP_KEY,
@@ -36,7 +38,8 @@ window.Echo = new Echo({
     auth: store.getters.authenticationHeaders,
 });
 
-window.sweetAlert = require('sweetalert');
+import sweetAlert from 'sweetalert';
+window.sweetAlert = sweetAlert;
 window.sweetAlert.setDefaults({
                 confirmButtonText: 'Si',
                 cancelButtonText: 'No',
